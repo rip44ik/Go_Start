43141906136582111abc90b00504745d9ceb7bf0
rip4ik <xfx.life@gmail.com>
2023-12-16 21:34:20 +0300

    завершил игру

--- game.go
+++ game.go
@@ -1,3 +1,4 @@
+// guess - игра, в которой игрок должен угадать случайное число
 package main
 
 import (
@@ -14,10 +15,10 @@ func main() {
 	target := rand.Intn(100) + 1
 	fmt.Println("Загадано число от 1 до 100")
 	fmt.Println("Угадайте его за 10 попыток. Готовы ?")
-	fmt.Println(target)
 
 	reader := bufio.NewReader(os.Stdin)
 
+	success := false
 	for guesses := 0; guesses < 10; guesses++ {
 		fmt.Println("Вы имеете", 10-guesses, "попыток.")
 
@@ -36,8 +37,12 @@ func main() {
 		} else if guess < target {
 			fmt.Println("Упс,загаданое число БОЛЬШЕ.")
 		} else {
+			success = true
 			fmt.Println("Вы угадали! за", guesses+1, "попыток")
 			break
 		}
+	}
+	if !success {
+		fmt.Println("Извините вы не угадали загаданное число. Загаданное число - ", target)
 	}
 }
